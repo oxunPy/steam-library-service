9e7d4b9cfb58d2a094ec8393707ab118be65218f
acer <csharpoxun@gmail.com>
2025-02-21 15:02:18 +0500

    steam-chat-service. steam-matchmaking-service. steam-notification-service. steam-review-service. steam-store-service. steam-user-service. steam-library-service. steam-game-service.

---
+++ src/main/java/org/example/steamlibraryservice/SteamLibraryServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.steamlibraryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamLibraryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamLibraryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/steamlibraryservice/SteamLibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.steamlibraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamLibraryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
